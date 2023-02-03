09e327a8ca53b0e8742355476908b0ed37addd41
Anuj Sthapit <anujsthapit30@gmail.com>
2023-02-03 14:24:29 +0545

    backendserver

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -22,7 +22,7 @@ class Register extends React.Component  {
     this.setState({ password: event.target.value });
   };
   onSubmitSignIn = () => {
-    fetch("git@github.com:I1hpI/smartbrain-backend.git/register", {
+    fetch("https://smartbrain-backend-anf7.onrender.com/register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -16,7 +16,7 @@ class Signin extends React.Component {
   };
   onSubmitSignIn = () => {
     // fetch("http://localhost:3000/signin",
-    fetch("git@github.com:I1hpI/smartbrain-backend.git/signin",
+    fetch("https://smartbrain-backend-anf7.onrender.com/signin",
      {
       method: "post",
       headers: { "Content-Type": "application/json" },
