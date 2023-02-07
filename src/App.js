80a57603667d331a7530623ff209da4e23dd2ada
Anuj Sthapit <anujsthapit30@gmail.com>
2023-02-07 16:53:14 +0545

    urlfixed

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://smartbrain-backend-anf7.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
